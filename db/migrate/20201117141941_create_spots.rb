22409b242375b3c54af9f25581998fe1f09b2481
hisadomi <hb.kmaia93@icloud.com>
2020-11-17 23:21:59 +0900

    Area, Spotモデル作成、アソシエーション追加

---
+++ db/migrate/20201117141941_create_spots.rb
@@ -0,0 +1,12 @@
+class CreateSpots < ActiveRecord::Migration[6.0]
+  def change
+    create_table :spots do |t|
+      t.references :area_id, null: false, foreign_key: true
+      t.string :name
+      t.integer :stay_time
+      t.string :image
+
+      t.timestamps
+    end
+  end
+end
