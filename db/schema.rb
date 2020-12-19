d6cc8e82a9888a3a000bd3d85920fcf6e8a72955
hisadomi <hb.kmaia93@icloud.com>
2020-12-19 17:23:51 +0900

    MyScheduleモデルのcourse_numberカラム削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_08_103814) do
+ActiveRecord::Schema.define(version: 2020_12_19_073721) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,7 +44,6 @@ ActiveRecord::Schema.define(version: 2020_12_08_103814) do
 
   create_table "model_courses", force: :cascade do |t|
     t.bigint "area_id", null: false
-    t.integer "course_number"
     t.float "score"
     t.float "distance"
     t.datetime "created_at", precision: 6, null: false
