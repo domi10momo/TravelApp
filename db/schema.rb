44339c491730ce9d617a072d74d378f867109b1d
hisadomi <hb.kmaia93@icloud.com>
2020-12-24 00:03:35 +0900

    観光地情報を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_23_015937) do
+ActiveRecord::Schema.define(version: 2020_12_23_071528) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -88,6 +88,7 @@ ActiveRecord::Schema.define(version: 2020_12_23_015937) do
     t.string "image"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "description"
     t.index ["area_id"], name: "index_spots_on_area_id"
   end
 
