a45e59d6b7b73c78080674c702aae24d605a276c
hisadomi <hb.kmaia93@icloud.com>
2021-01-10 13:41:01 +0900

    rubocop警告修正

--- app/models/my_schedule.rb
+++ app/models/my_schedule.rb
@@ -1,12 +1,12 @@
 class MySchedule < ApplicationRecord
   has_many :my_travel_courses, dependent: :destroy
   belongs_to :user
-  has_many :impressions
-  scope :me, -> (user) { where(user_id: user.id) }
-  scope :gone_flag, -> (gone) { where(gone: gone) }
-  scope :is_gone, -> (format) { find(format).update(gone: true) }
-  scope :find_id, -> (model) { find(model.my_schedule_id) }
-  scope :travel_date, -> (model) { find_id(model).date}
+  has_many :impressions, dependent: :destroy
+  scope :me, ->(user) { where(user_id: user.id) }
+  scope :gone_flag, ->(gone) { where(gone: gone) }
+  scope :is_gone, ->(format) { find(format).update(gone: true) }
+  scope :find_id, ->(model) { find(model.my_schedule_id) }
+  scope :travel_date, ->(model) { find_id(model).date }
 
   class << self
     def user_schedules(user, gone)
