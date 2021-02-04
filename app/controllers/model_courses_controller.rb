64fc6e68e196cdf8ea16c24732f7866fa5ac3a4d
hisadomi <hb.kmaia93@icloud.com>
2021-02-04 23:25:08 +0900

    冗長構文、コメント削除、画像追加

--- app/controllers/model_courses_controller.rb
+++ app/controllers/model_courses_controller.rb
@@ -9,8 +9,6 @@ class ModelCoursesController < ApplicationController
     # 選択したエリアのコースリストを選択
     @courses_in_area = ModelCourse.where(area_id: area_id).order(score: "ASC").page(params[:page]).per(10)
     @model_routes = CourseRoute.course_ids(@courses_in_area)
-    # @want_spots = current_user.wanted_spots
-    # @courses_in_area = Want.create_calc_course_score(@courses_in_area, @model_routes, @want_spots)
   end
 
   private
--- app/models/want.rb
+++ app/models/want.rb
@@ -5,19 +5,6 @@ class Want < ApplicationRecord
   validates :spot_id, presence: true
 
   class << self
-    def create_calc_course_score(model_courses, model_routes, want_spots)
-      # model_courses.each do |course|
-      #   route_spot_id = model_routes.where(model_course_id: course.id).pluck(:spot_id)
-
-      #   if route_spot_id.any? { |i| want_spots.ids.include?(i) }
-      #     course.update!(score: course.distance * 0.1)
-      #   else
-      #     course.update!(score: course.distance)
-      #   end
-      # end
-      # model_courses.order(score: "ASC")
-    end
-
     def change_score(courses, routes, param_spot_id)
       param_id = []
       param_id << param_spot_id.to_i
